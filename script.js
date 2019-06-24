6ccf5acc30f0c7d15de2031288862dd60e63539e
Leonardo Zambão <37703983+leonardozambao@users.noreply.github.com>
2019-06-24 10:56:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ $( window ).load(function() {
 
 // mascaras
 $("#telefone").mask("(99) 9999-99999");
+
+// efeito em elemento block
+
+$('#my-block').slideToggle('medium', function() {
+    if ($(this).is(':visible'))
+        $(this).css('display','inline-block');
+});
