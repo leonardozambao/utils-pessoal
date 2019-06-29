ded7a3682f6138cf7c027bd96cd9bbfbbf140202
Leonardo - notebook <leonardozambao@live.com>
2019-06-29 01:49:55 -0300

    jquery + efeito de scroll

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ $('form #cnpj').on('focus', function(){
     if ( $("form #cpf").val().length == 14){
         $('#cnpj').attr("disabled", true);}
 });
+
+// controlar classe no header com base no scroll da pagina
+
+$(window).scroll(function (e) {
+    if ($(window).width() > 768) {
+        var scroll = $(this).scrollTop();
+        divSelect(scroll);
+    }
+});
+
+var lastScrollTop = 0;
+function divSelect(scroll){
+    if (scroll > lastScrollTop){
+        //descendo scroll
+        if (scroll > $('header').innerHeight() - 160){
+            $('header').removeClass('show').addClass('no-top');
+        }else{
+            $('header').addClass('show').removeClass('no-top');
+        }
+    }
+    else{
+        //subindo scroll
+        if (scroll > $('header').innerHeight()){
+            $('header').addClass('no-top');
+        }else{
+            $('header').removeClass('no-top');
+        }
+        $('header').addClass('show');
+    }
+    lastScrollTop = scroll;
+}
