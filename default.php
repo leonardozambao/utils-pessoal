bd342564cb919274174d257e4251ce319c2b8724
Leonardo Zambão <37703983+leonardozambao@users.noreply.github.com>
2019-07-24 15:57:39 -0300

    Create default.php

---
+++ default.php
@@ -0,0 +1,5 @@
+<?php 
+
+?>
+<!-- forçar arquivo sem cache -->
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory')?>/landing_page_plano_expansao/style.css?=<?php echo date('dmyhis'); ?>" />
